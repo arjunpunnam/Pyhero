d8193451344d31b9f81ca89b4f81312876a7206a
arjunpunnam <nagarjuna.punnam@gmail.com>
2015-11-16 01:10:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ app.config.update(
 )
 
 # controllers
-@app.route("/")
-def hello():
-    return "Hello from Python!"
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
-# launch
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)app.py
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route("/")
+def index():
+    return render_template('index.html')
